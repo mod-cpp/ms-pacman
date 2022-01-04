9995b167ad01ff4a43dfcfc878ce73f2d5d3b7a7
Patricia Aas <patricia@turtlesec.no>
2022-01-04 11:24:06 +0100

    Fix namespace name in Objective-C file

--- scaling-lib/Scaling.mm
+++ scaling-lib/Scaling.mm
@@ -6,7 +6,7 @@ It is not a C++ file and is not part of this course!
 #import "AppKit/NSWindow.h"
 #include "Scaling.hpp"
 
-namespace pacman {
+namespace ms_pacman {
 
 double scaling_factor_for_window(sf::WindowHandle handle) {
     return [ reinterpret_cast<id>(handle) backingScaleFactor];
