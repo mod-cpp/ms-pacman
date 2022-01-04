adf8a5f2d58b765f8daa940fffcc5348773961eb
Corentin Jabot <corentinjabot@gmail.com>
2022-01-04 11:43:15 +0100

    Always set the toolchain file

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -12,9 +12,9 @@ if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
     if(NOT vcpkg_POPULATED)
         FetchContent_Populate(vcpkg)
     endif()
-
-    set(CMAKE_TOOLCHAIN_FILE
-            "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
-    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE
-            "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
 endif()
+
+set(CMAKE_TOOLCHAIN_FILE
+"${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE
+"${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
