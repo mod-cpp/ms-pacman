5160d9c5db19fda4ea24d62df41359542156f02c
Corentin Jabot <corentinjabot@gmail.com>
2022-01-05 22:43:45 +0100

    Make Position a generic type

--- lib/include/Position.hpp
+++ lib/include/Position.hpp
@@ -3,21 +3,26 @@
 #include <cassert>
 #include <cmath>
 #include <limits>
+#include <type_traits>
 
 namespace ms_pacman {
 
-struct Position {
-  double x = 0.0;
-  double y = 0.0;
-};
+template <typename T>
+concept number = std::is_integral_v<T> || std::is_floating_point_v<T>;
 
-struct GridPosition {
-  size_t x;
-  size_t y;
-  constexpr GridPosition(size_t x, size_t y)
+template <number T>
+struct BasicPosition {
+  T x = {};
+  T y = {};
+  constexpr BasicPosition() = default;
+  constexpr BasicPosition(T x, T y)
     : x(x),
       y(y) {}
 };
+
+using Position = BasicPosition<double>;
+using GridPosition = BasicPosition<std::size_t>;
+
 
 inline GridPosition positionToGridPosition(Position pos) {
   assert(pos.x >= 0 && pos.y >= 0 && "Position should have positive values");
@@ -32,18 +37,17 @@ constexpr bool operator==(const GridPosition & a, const GridPosition & b) {
   return a.x == b.x && a.y == b.y;
 }
 
-constexpr bool operator!=(const GridPosition & a, const GridPosition & b) {
-  return !(a == b);
+template<typename T>
+inline double positionDistance(const T & a, const T & b) {
+  const double first = double(a.x) - double(b.x);
+  const double second = double(a.y) - double(b.y);
+  return std::sqrt((first * first) + (second * second));
 }
 
 inline bool operator==(const Position & a, const Position & b) {
   // This is ok as a test unless x and y become very large.
   constexpr double epsilon = std::numeric_limits<double>::epsilon();
   return std::abs(a.x - b.x) <= epsilon && std::abs(a.y - b.y) <= epsilon;
-}
-
-inline bool operator!=(const Position & a, const Position & b) {
-  return !(a == b);
 }
 
 } // namespace pacman
