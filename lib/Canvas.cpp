259d363c72799003e73454e0d2781daa4a31db10
Patricia Aas <psmaas@gmail.com>
2022-03-22 13:50:45 +0100

    Move implementation into the cpp file

--- lib/Canvas.cpp
+++ lib/Canvas.cpp
@@ -113,6 +113,35 @@ void Canvas::renderPellets(const DefaultBoard & board) {
   }
 }
 
+
+
+void Canvas::render(const GenericFruit & fruit, std::span<const GenericFruit> eatenFruits) {
+  Sprite sprite = getSprite(Fruits::sprite(fruit));
+  if (Fruits::isVisible(fruit)) {
+    const auto & pos = Fruits::position(fruit);
+    render(sprite, pos);
+  }
+
+  auto render_fruit = [this](int position, Sprite fruit_sprite) {
+    const auto x = static_cast<size_t>(LEFT_MARGIN + TARGET_MAZE_WIDTH + LEFT_MARGIN);
+    const auto y = static_cast<size_t>((TARGET_MAZE_HEIGHT / 3.0) * 2);
+    const auto sprite_position = float(position) * SPRITE_WIDTH * 1.5f;
+    const sf::Vector2f pos{ x + sprite_position, y };
+    fruit_sprite.setPosition(pos.x, pos.y);
+    window.draw(fruit_sprite);
+  };
+
+  int position = 0;
+  for (const auto & eatenFruit : eatenFruits) {
+    GridPosition current_sprite = std::visit([](auto && fruit) { return fruit.sprite(); }, eatenFruit);
+    Sprite eaten_sprite = getSprite(current_sprite);
+    render_fruit(position, eaten_sprite);
+    position++;
+  }
+
+  render_fruit(position, sprite);
+}
+
 void Canvas::render(const MsPacMan & pac_man) {
   Sprite pacmanSprite = getSprite(pac_man.currentSprite());
   const auto & pos = pac_man.position();
--- lib/include/Canvas.hpp
+++ lib/include/Canvas.hpp
@@ -50,32 +50,7 @@ private:
 
   void renderPellets(const DefaultBoard & board);
 
-  void render(const GenericFruit & fruit, std::span<const GenericFruit> eatenFruits) {
-    Sprite sprite = getSprite(Fruits::sprite(fruit));
-    if (Fruits::isVisible(fruit)) {
-      const auto & pos = Fruits::position(fruit);
-      render(sprite, pos);
-    }
-
-    auto render_fruit = [this](int position, Sprite fruit_sprite) {
-      const auto x = static_cast<size_t>(LEFT_MARGIN + TARGET_MAZE_WIDTH + LEFT_MARGIN);
-      const auto y = static_cast<size_t>((TARGET_MAZE_HEIGHT / 3.0) * 2);
-      const auto sprite_position = float(position) * SPRITE_WIDTH * 1.5f;
-      const sf::Vector2f pos{ x + sprite_position, y };
-      fruit_sprite.setPosition(pos.x, pos.y);
-      window.draw(fruit_sprite);
-    };
-
-    int position = 0;
-    for (const auto & eatenFruit : eatenFruits) {
-      GridPosition current_sprite = std::visit([](auto && fruit) { return fruit.sprite(); }, eatenFruit);
-      Sprite eaten_sprite = getSprite(current_sprite);
-      render_fruit(position, eaten_sprite);
-      position++;
-    }
-
-    render_fruit(position, sprite);
-  }
+  void render(const GenericFruit & fruit, std::span<const GenericFruit> eatenFruits);
 
   void renderScore(int score);
   void renderLives(int lives);
