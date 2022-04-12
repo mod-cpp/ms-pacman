d7c7272925cb24d4705a357ed239dfb18d0cf30c
Patricia Aas <patricia@turtlesec.com>
2022-04-12 21:58:13 +0200

    Exercise 341

--- test/exercises/34_exercises.cpp
+++ test/exercises/34_exercises.cpp
@@ -2,8 +2,40 @@
 
 // To enable a test remove [.] from the tags on the test
 
+constexpr void unused(auto X) { (void) X; }
+
 // 34. Class templates, function templates, non-type parameters
 
-TEST_CASE("Exercise 341 : ", "[.][34]") {
-  REQUIRE(true == false);
+template<typename T>
+class set {
+public:
+  std::size_t size() const {
+    return items.size();
+  }
+
+  bool contains(const T & item) const {
+    return std::any_of(items.begin(), items.end(), [&item](T current){ return current == item; });
+  }
+
+  void operator<<(const T & item) {
+    unused(item);
+    // Exercise 341
+    // Hint: https://en.cppreference.com/w/cpp/container/vector/push_back
+  }
+private:
+  std::vector<T> items;
+};
+
+// Only add items not already in the set
+TEST_CASE("Exercise 341 : Implement streaming operator as insert into the set", "[.][34]") {
+  set<int> int_set;
+  CHECK(int_set.size() == 0);
+  int_set << 42;
+  CHECK(int_set.size() == 1);
+  int_set << 43;
+  CHECK(int_set.size() == 2);
+  int_set << 42;
+  CHECK(int_set.size() == 2);;
+  int_set << 43;
+  CHECK(int_set.size() == 2);
 }
