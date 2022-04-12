9661944c74255743514e9c2e6f006ed708def5aa
Patricia Aas <patricia@turtlesec.com>
2022-04-12 19:13:32 +0200

    Try to turn off tests with an ifdef

--- test/exercises/32_exercises.cpp
+++ test/exercises/32_exercises.cpp
@@ -1,7 +1,15 @@
 #include <catch2/catch.hpp>
 
+//#define RUN_TESTS
+
+#ifdef RUN_TESTS
+#define DISABLE ""
+#else
+#define DISABLE "[.]"
+#endif
+
 // 32. Overloading
 
-TEST_CASE("32 Demo", "[!shouldfail][32]") {
+TEST_CASE("32 Demo", DISABLE"[32]") {
   REQUIRE(true == false);
 }
