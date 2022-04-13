d8af6d3489978518006dd14de0dff4c119cc84a8
Patricia Aas <patricia@turtlesec.com>
2022-04-13 03:21:04 +0200

    Add Exercise 34

--- test/exercises/34_exercises.cpp
+++ test/exercises/34_exercises.cpp
@@ -40,8 +40,40 @@ TEST_CASE("Exercise 341 : Implement streaming operator as insert into the set", 
   CHECK(int_set.size() == 2);
   int_set << 42;
   CHECK(int_set.size() == 2);
-  ;
   int_set << 43;
   CHECK(int_set.size() == 2);
 }
+
+template <typename T>
+static T accumulate(std::vector<T> vec, T init) {
+  unused(vec);
+  // Exercise 342
+  // Hint: https://en.cppreference.com/w/cpp/algorithm/accumulate
+  return init;
+}
+
+TEST_CASE("Exercise 342 : Implement a function template", "[.][34]") {
+std::vector<int> int_vec = { 21, 32, 43, 54 };
+CHECK(accumulate(int_vec, 0) == (21 + 32 + 43 + 54));
+
+std::vector<std::string> string_vec = { "21", "32", "43", "54" };
+CHECK(accumulate(string_vec, { "" }) == "21324354");
+}
+
+static auto acc(auto vec, auto init) {
+  unused(vec);
+  // Exercise 343
+  // Hint: https://en.cppreference.com/w/cpp/algorithm/accumulate
+  return init;
+}
+
+TEST_CASE("Exercise 343 : Implement an abbreviated function template", "[.][34]") {
+  std::vector<int> int_vec = { 21, 32, 43, 54 };
+  CHECK(acc(int_vec, 0) == (21 + 32 + 43 + 54));
+
+  std::vector<std::string> string_vec = { "21", "32", "43", "54" };
+  std::string init = "";
+  CHECK(acc(string_vec, init) == "21324354");
+}
+
 } // namespace exercises_34
--- test/exercises/34_exercises.md
+++ test/exercises/34_exercises.md
@@ -5,3 +5,9 @@
 ## Exercise 340:
 
 ## Exercise 341 : Implement streaming operator as insert into the set
+
+## Exercise 342 : Implement a function template
+
+## Exercise 343 : Implement an abbreviated function template
+
+Considering exercise 342 and 343 - how are they different? Will they match the same signatures?
