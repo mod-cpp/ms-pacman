63d7df951a825af980c83422219c78f7a063f470
Patricia Aas <patricia@turtlesec.com>
2022-04-13 23:24:50 +0200

    Add Exercise 46

--- test/exercises/46_exercises.cpp
+++ test/exercises/46_exercises.cpp
@@ -5,8 +5,59 @@
 // 46. Pragmatic introduction to move semantics and return value optimizations
 namespace exercises_46 {
 
-TEST_CASE("Exercise 461 : ", "[.][46]") {
-  REQUIRE(true == false);
+constexpr void unused(auto X) {
+  (void)X;
 }
+
+struct Point {
+  int x = 0;
+  int y = 0;
+};
+
+struct Size {
+  size_t width = 0;
+  size_t height = 0;
+};
+
+struct Rect {
+  size_t width() const { return size.width; }
+  size_t height() const { return size.height; }
+  int x() const { return point.x; }
+  int y() const { return point.y; }
+  int right() const { return point.x + int(size.width); }
+  int bottom() const { return point.y + int(size.height); }
+
+  Point point;
+  Size size;
+};
+
+static Rect bounding_box(const Rect & one, const Rect & two) {
+  unused(one);
+  unused(two);
+  return { { 0, 0 }, { 0, 0 } };
+}
+
+TEST_CASE("Exercise 461 : Implement bounding_box", "[.][46]") {
+  {
+    Rect one = { { 1, 1 }, { 3, 2 } };
+    Rect two = { { 3, 4 }, { 3, 3 } };
+    Rect box = bounding_box(one, two);
+    CHECK(box.x() == 1);
+    CHECK(box.y() == 1);
+    CHECK(box.width() == 5);
+    CHECK(box.height() == 6);
+  }
+  {
+    Rect one = { { 1, 1 }, { 3, 2 } };
+    Rect two = { { 3, 4 }, { 3, 3 } };
+    Rect box = bounding_box(two, one);
+    CHECK(box.x() == 1);
+    CHECK(box.y() == 1);
+    CHECK(box.width() == 5);
+    CHECK(box.height() == 6);
+  }
+}
+
+// TODO Tests on move and RVO and NRVO
 
 } // namespace exercises_46
--- test/exercises/46_exercises.md
+++ test/exercises/46_exercises.md
@@ -3,3 +3,17 @@
 # 46. Pragmatic introduction to move semantics and return value optimizations
 
 ## Exercise 460: 
+
+## Exercise 461 : Implement bounding_box
+
+```cpp
+static Rect bounding_box(const Rect & one, const Rect & two) {
+  int x = std::min(one.x(), two.x());
+  int y = std::min(one.y(), two.y());
+  int right = std::max(one.right(), two.right());
+  int bottom = std::max(one.bottom(), two.bottom());
+  auto width = static_cast<size_t>(right - x);
+  auto height = static_cast<size_t>(bottom - y);
+  return { { x, y }, { width, height } };
+}
+```
