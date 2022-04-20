648371dcbf37bffd5059af3031435c926db5da90
Patricia Aas <patricia@turtlesec.com>
2022-04-20 13:43:21 +0200

    Add tests for 38

--- test/exercises/38_exercises.cpp
+++ test/exercises/38_exercises.cpp
@@ -5,8 +5,35 @@
 // 38. Variadic templates, fold expressions and parameter packs
 namespace exercises_38 {
 
-TEST_CASE("Exercise 381 : ", "[.][38]") {
-  REQUIRE(true == false);
+// Note use of comma operator
+constexpr void unused(auto ...X) {
+  (..., (void)X);
+}
+
+constexpr bool any_of(auto ...item) {
+  unused(item...);
+  return false;
+}
+
+TEST_CASE("Exercise 381 : Implement any_of", "[.][38]") {
+  REQUIRE(true == any_of(false, false, true));
+  REQUIRE(false == any_of(false, false, false));
+  REQUIRE(true == any_of(true));
+  REQUIRE(false == any_of(false));
+  REQUIRE(true == any_of(true, true, true));
+}
+
+constexpr bool all_of(auto ...item) {
+  unused(item...);
+  return false;
+}
+
+TEST_CASE("Exercise 382 : Implement all_of", "[.][38]") {
+  REQUIRE(false == all_of(false, false, true));
+  REQUIRE(false == all_of(false, false, false));
+  REQUIRE(true == all_of(true));
+  REQUIRE(false == all_of(false));
+  REQUIRE(true == all_of(true, true, true));
 }
 
 } // namespace exercises_38
--- test/exercises/38_exercises.md
+++ test/exercises/38_exercises.md
@@ -3,7 +3,35 @@
 # 38. Variadic templates, fold expressions and parameter packs
 
 * [Exercise 380](#exercise-380)
+* [Exercise 381](#exercise-381)
+* [Exercise 382](#exercise-382)
 
-## [Exercise 380][1]
+## Exercise 380
+
+## [Exercise 381][1]
+### Implement any_of
+
+<details>
+   <summary>Solution</summary>
+
+```cpp
+constexpr bool any_of(auto ...item) {
+  return (item || ...);
+}
+```
+</details>
+
+## [Exercise 382][1]
+### Implement all_of
+
+<details>
+   <summary>Solution</summary>
+
+```cpp
+constexpr bool all_of(auto ...item) {
+  return (item && ...);
+}
+```
+</details>
 
 [1]: 38_exercises.cpp
