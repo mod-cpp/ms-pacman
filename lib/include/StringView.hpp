0a8a0b815b96feffcd735745dddb79ca71a99dff
Patricia Aas <patricia@turtlesec.no>
2022-05-10 15:06:10 +0200

    Use inline

--- lib/include/StringView.hpp
+++ lib/include/StringView.hpp
@@ -6,18 +6,18 @@
 
 namespace ms_pacman::StringView {
 
-std::string to_upper(std::string_view /*word*/) {
+inline std::string to_upper(std::string_view /*word*/) {
   std::string ret;
   // Make whole word uppercase
   return ret;
 }
 
-std::string trim(std::string_view /*view*/) {
+inline std::string trim(std::string_view /*view*/) {
   // Exercise 422 : Trim leading and trailing white space
   return {};
 }
 
-std::vector<std::string> split(std::string_view /*view*/) {
+inline std::vector<std::string> split(std::string_view /*view*/) {
   std::vector<std::string> tokens;
   // Split view into tokens based on whitespace
   return tokens;
