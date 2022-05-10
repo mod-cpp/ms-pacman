91516caaf30023cd52befef0ca6ecd7165ad4df8
Patricia Aas <psmaas@gmail.com>
2022-05-10 19:05:09 +0200

    Move get_player_score (fixup)

--- lib/HighScore.cpp
+++ lib/HighScore.cpp
@@ -1,10 +1,9 @@
 #include "HighScore.hpp"
 #include "HighScoreFile.hpp"
 #include "LineStream.hpp"
-#include "String.hpp"
 #include "User.hpp"
+#include "StringView.hpp"
 
-#include <algorithm>
 #include <cstdlib>
 #include <fmt/printf.h>
 #include <memory>
@@ -41,7 +40,7 @@ void HighScore::insert(const std::string & name, int score) {
 static std::tuple<std::string, int> get_player_score(const std::string & line) {
   std::vector<std::string> parts = StringView::split(line, ',');
   auto player_name = StringView::to_upper(StringView::trim(parts[0]));
-  auto player_score = StringView::to_int(parts[1]);
+  auto player_score = StringView::to_int(StringView::trim(parts[1]));
   return { player_name, player_score };
 }
 
