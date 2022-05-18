7c757000892a97094a824bd449564b5b4f30822b
Patricia Aas <patricia@turtlesec.no>
2022-05-18 17:08:20 +0200

    Adapt PR for merging

---
+++ lib/NPC_23.cpp
@@ -0,0 +1,73 @@
+#include "NPC.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <vector>
+
+namespace ms_pacman {
+
+void NPC::updateDirection(const DefaultBoard & board) {
+  const auto current_grid_position = positionInGrid();
+  if (isSamePosition(current_grid_position, last_grid_position))
+    return;
+
+  struct Move {
+    Direction direction = Direction::NONE;
+    Position position;
+    double distance_to_target = std::numeric_limits<double>::infinity();
+  };
+
+  const auto [x, y] = gridPositionToPosition(current_grid_position);
+  std::array<Move, 4> possible_moves = {
+    Move{ Direction::UP, { x, y - 1 } },
+    Move{ Direction::LEFT, { x - 1, y } },
+    Move{ Direction::DOWN, { x, y + 1 } },
+    Move{ Direction::RIGHT, { x + 1, y } }
+  };
+
+  auto set_teleport = [&](const auto & move) {
+    Move transformed = move;
+    if (shouldTeleport(board, current_grid_position, move.direction))
+      transformed.position = gridPositionToPosition(teleport(board, current_grid_position));
+    return transformed;
+  };
+
+  auto has_valid_position = [](const auto & move) {
+    return !(move.position.x < 0 || move.position.y < 0);
+  };
+
+  auto has_valid_direction = [&](const auto & move) {
+    return !(move.direction == oppositeDirection(direction));
+  };
+
+  auto is_allowed_move = [&](const auto & move) {
+    const GridPosition grid_position = { size_t(move.position.x), size_t(move.position.y) };
+    return isWalkable(board, current_grid_position, grid_position);
+  };
+
+  auto set_distance_to_target = [&](const auto & move) {
+    Move transformed = move;
+    transformed.distance_to_target = std::hypot(move.position.x - target.x, move.position.y - target.y);
+    return transformed;
+  };
+
+  auto view = possible_moves
+              | std::views::transform(set_teleport)
+              | std::views::filter(has_valid_position)
+              | std::views::filter(has_valid_direction)
+              | std::views::filter(is_allowed_move)
+              | std::views::transform(set_distance_to_target);
+
+  if (std::ranges::empty(view))
+    return;
+
+  const auto optimal_move = std::ranges::min_element(view, {}, &Move::distance_to_target);
+
+  const auto & move = *optimal_move;
+  direction = move.direction;
+  last_grid_position = current_grid_position;
+}
+
+} // namespace ms_pacman
--- lib/include/NPC.hpp
+++ lib/include/NPC.hpp
@@ -2,12 +2,7 @@
 
 #include "Board.hpp"
 
-#include <algorithm>
 #include <chrono>
-#include <numeric>
-#include <ranges>
-#include <string>
-#include <vector>
 
 namespace ms_pacman {
 
@@ -82,67 +77,7 @@ public:
    *  In the scatter state, each npc tries to reach an unreachable position outside of the map.
    *  This makes npcs run in circle around the island at each of the 4 map corner.
    */
-  void updateDirection(const DefaultBoard & board) {
-    const auto current_grid_position = positionInGrid();
-    if (isSamePosition(current_grid_position, last_grid_position))
-      return;
-
-    struct Move {
-      Direction direction = Direction::NONE;
-      Position position;
-      double distance_to_target = std::numeric_limits<double>::infinity();
-    };
-
-    const auto [x, y] = gridPositionToPosition(current_grid_position);
-    std::array<Move, 4> possible_moves = {
-      Move{ Direction::UP, { x, y - 1 } },
-      Move{ Direction::LEFT, { x - 1, y } },
-      Move{ Direction::DOWN, { x, y + 1 } },
-      Move{ Direction::RIGHT, { x + 1, y } }
-    };
-
-    auto set_teleport = [&](const auto & move) {
-      Move transformed = move;
-      if (shouldTeleport(board, current_grid_position, move.direction))
-        transformed.position = gridPositionToPosition(teleport(board, current_grid_position));
-      return transformed;
-    };
-
-    auto has_valid_position = [](const auto & move) {
-      return !(move.position.x < 0 || move.position.y < 0);
-    };
-
-    auto has_valid_direction = [&](const auto & move) {
-      return !(move.direction == oppositeDirection(direction));
-    };
-
-    auto is_allowed_move = [&](const auto & move) {
-      const GridPosition grid_position = { size_t(move.position.x), size_t(move.position.y) };
-      return isWalkable(board, current_grid_position, grid_position);
-    };
-
-    auto set_distance_to_target = [&](const auto & move) {
-      Move transformed = move;
-      transformed.distance_to_target = std::hypot(move.position.x - target.x, move.position.y - target.y);
-      return transformed;
-    };
-
-    auto view = possible_moves
-                | std::views::transform(set_teleport)
-                | std::views::filter(has_valid_position)
-                | std::views::filter(has_valid_direction)
-                | std::views::filter(is_allowed_move)
-                | std::views::transform(set_distance_to_target);
-
-    if (std::ranges::empty(view))
-      return;
-
-    const auto optimal_move = std::ranges::min_element(view, {}, &Move::distance_to_target);
-
-    const auto & move = *optimal_move;
-    direction = move.direction;
-    last_grid_position = current_grid_position;
-  }
+  void updateDirection(const DefaultBoard & board);
 
   Position position() const {
     return pos;
