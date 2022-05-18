3df8effc0b4758c1f3d029b8a7381a0610012413
Patricia Aas <patricia@turtlesec.no>
2022-05-18 17:32:04 +0200

    Adapt PR for merging

---
+++ cmake/ranges.cmake
@@ -0,0 +1,13 @@
+option(ENABLE_RANGES "Enable ranges" FALSE)
+
+if (WIN32)
+    set(ENABLE_RANGES YES)
+endif ()
+
+if (CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 11.0)
+    set(ENABLE_RANGES YES)
+endif()
+
+if (ENABLE_RANGES)
+    message(STATUS "${CMAKE_PROJECT_NAME}: Enabling use of ranges")
+endif ()
--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -4,8 +4,3 @@ set_project_warnings()
 include(${CMAKE_CURRENT_LIST_DIR}/Sanitizers.cmake)
 set(ENABLE_SANITIZER_ADDRESS YES)
 enable_sanitizers()
-
-option(ENABLE_RANGES "Enable ranges" FALSE)
-if (WIN32)
-    set(ENABLE_RANGES YES)
-endif ()
