6907318b603f4c1eec67ae67dc2e714f334fbf3d
Patricia Aas <patricia@turtlesec.no>
2022-08-27 15:40:30 +0200

    Remove line, it seems to cause issues on 18.04 at least

--- dev/ubuntu.sh
+++ dev/ubuntu.sh
@@ -36,7 +36,6 @@ CODE_NAME=`lsb_release -cs`
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ ${CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update
-sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
 sudo apt-get install kitware-archive-keyring
 sudo apt-get install cmake
 
