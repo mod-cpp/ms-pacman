950974aad112073a34c703ed3851b5c454311391
Patricia Aas <patricia@turtlesec.no>
2024-03-20 20:34:07 +0100

    Add -y to apt install in script

--- dev/up_to_date_cmake.sh
+++ dev/up_to_date_cmake.sh
@@ -10,5 +10,5 @@ CODE_NAME=`lsb_release -cs`
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ ${CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update
-sudo apt-get install kitware-archive-keyring
-sudo apt-get install cmake
+sudo apt-get install -y kitware-archive-keyring
+sudo apt-get install -y cmake
